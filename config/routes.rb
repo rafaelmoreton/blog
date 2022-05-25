c4c420dce8a84ae3098720c4af121c7d12fe3bf6
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-05-25 12:03:16 -0300

    Feat: Read articles and create new ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'articles/index', to: "articles#index"
-
   # Defines the root path route ("/")
   # root "articles#index"
   root "articles#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
