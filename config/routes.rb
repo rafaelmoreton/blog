9c222035218fc4009d0a38f4d8c9bc1ac6fb108b
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-05-26 12:44:20 -0300

    Feat: Add comments to articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
